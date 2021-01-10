a34e57292a169c5ce5d04f90c8caf44f12a4479b
Gwen Boatrite <gwen.boatrite@gmail.com>
2021-01-10 15:39:53 -0600

    Refactor coordinate lines renderer
    I only need a single vao and vbo and one draw call when I put the color
    in the vertices all in a single array.

--- src/renderers/coordinate_lines_renderer.h
+++ src/renderers/coordinate_lines_renderer.h
@@ -9,84 +9,41 @@ class CoordinateLinesRenderer : public Renderer {
     std::shared_ptr<Camera> m_camera_ptr;
     Shader m_shader { Shader("coordinate_lines.vert", "coordinate_lines.frag") };
 
-    unsigned int m_x_axis_vao {};
-    unsigned int m_x_axis_vbo {};
-    unsigned int m_y_axis_vao {};
-    unsigned int m_y_axis_vbo {};
-    unsigned int m_z_axis_vao {};
-    unsigned int m_z_axis_vbo {};
+    unsigned int m_vao {};
+    unsigned int m_vbo {};
 
   public:
     CoordinateLinesRenderer(std::shared_ptr<Camera> camera_ptr) : m_camera_ptr { camera_ptr } {
-      // I need to get this working still, but once I do, I feel like I could
-      // simplify this code a bit. Instead of 3 VAOs, I could just use one in
-      // some default position, rotate it, color it, and render it. Is that
-      // better?
-      //
-      // I also want to do some sort of chunk border renderer and probably a
-      // block border renderer. What I'm actually thinking now is that I just
-      // build out a giant mesh and send all the vertices precomputed and stored
-      // in a single VBO. But idk if there is a gl render method for groups of
-      // lines...
-      //
-      // Okay yeah GL_LINES interprets the vertex stream as being distinct pairs
-      // of lines. So I can build up a grid or whatever.
-      //
-      // In which case, I could just have a single vertices array computed based
-      // off of a few things like length to make the axis (right now it's 512 in
-      // both directions).
-
-      float x_axis_vertices[6] {
-        -512.0f, 0.0f, 0.0f,
-          512.0f, 0.0f, 0.0f,
+      float length = 512.0f;
+      float vertices[6 * 6] {
+        // X
+        -length, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        length, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        // Y
+        0.0f, -length, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, length, 0.0f, 0.0f, 1.0f, 0.0f,
+        // Z
+        0.0f, 0.0f, -length, 0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, length, 0.0f, 0.0f, 1.0f,
       };
 
-      float y_axis_vertices[6] {
-        0.0f, -512.0f, 0.0f,
-          0.0f, 512.0f, 0.0f
-      };
+      glGenVertexArrays(1, &m_vao);
+      glGenBuffers(1, &m_vbo);
 
-      float z_axis_vertices[6] {
-        0.0f, 0.0f, -512.0f,
-          0.0f, 0.0f, 512.0f,
-      };
+      glBindVertexArray(m_vao);
+      glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-      glGenVertexArrays(1, &m_x_axis_vao);
-      glGenBuffers(1, &m_x_axis_vbo);
-
-      glGenVertexArrays(1, &m_y_axis_vao);
-      glGenBuffers(1, &m_y_axis_vbo);
-
-      glGenVertexArrays(1, &m_z_axis_vao);
-      glGenBuffers(1, &m_z_axis_vbo);
-
-      glBindVertexArray(m_x_axis_vao);
-      glBindBuffer(GL_ARRAY_BUFFER, m_x_axis_vbo);
-      glBufferData(GL_ARRAY_BUFFER, sizeof(x_axis_vertices), x_axis_vertices, GL_STATIC_DRAW);
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
       glEnableVertexAttribArray(0);
 
-      glBindVertexArray(m_y_axis_vao);
-      glBindBuffer(GL_ARRAY_BUFFER, m_y_axis_vbo);
-      glBufferData(GL_ARRAY_BUFFER, sizeof(y_axis_vertices), y_axis_vertices, GL_STATIC_DRAW);
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-      glEnableVertexAttribArray(0);
-
-      glBindVertexArray(m_z_axis_vao);
-      glBindBuffer(GL_ARRAY_BUFFER, m_z_axis_vbo);
-      glBufferData(GL_ARRAY_BUFFER, sizeof(z_axis_vertices), z_axis_vertices, GL_STATIC_DRAW);
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-      glEnableVertexAttribArray(0);
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+      glEnableVertexAttribArray(1);
     }
 
     ~CoordinateLinesRenderer() {
-      glDeleteBuffers(1, &m_x_axis_vbo);
-      glDeleteBuffers(1, &m_y_axis_vbo);
-      glDeleteBuffers(1, &m_z_axis_vbo);
-
-      glDeleteVertexArrays(1, &m_x_axis_vao);
-      glDeleteVertexArrays(1, &m_y_axis_vao);
-      glDeleteVertexArrays(1, &m_z_axis_vao);
+      glDeleteBuffers(1, &m_vbo);
+      glDeleteVertexArrays(1, &m_vao);
     }
 
     void render(double dt) override {
@@ -96,17 +53,8 @@ class CoordinateLinesRenderer : public Renderer {
       m_shader.setMat4("view", m_camera_ptr->getViewMatrix());
       m_shader.setMat4("projection", m_camera_ptr->getProjectionMatrix());
 
-      glBindVertexArray(m_x_axis_vao);
-      m_shader.setVec3("color", glm::vec3(1.0f, 0.0f, 0.0f));
-      glDrawArrays(GL_LINES, 0, 2);
-
-      glBindVertexArray(m_y_axis_vao);
-      m_shader.setVec3("color", glm::vec3(0.0f, 1.0f, 0.0f));
-      glDrawArrays(GL_LINES, 0, 2);
-
-      glBindVertexArray(m_z_axis_vao);
-      m_shader.setVec3("color", glm::vec3(0.0f, 0.0f, 1.0f));
-      glDrawArrays(GL_LINES, 0, 2);
+      glBindVertexArray(m_vao);
+      glDrawArrays(GL_LINES, 0, 6);
 
       glBindVertexArray(0);
     };
--- src/renderers/coordinate_lines_renderer/coordinate_lines.vert
+++ src/renderers/coordinate_lines_renderer/coordinate_lines.vert
@@ -1,6 +1,9 @@
 #version 330 core
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+out vec3 Color;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -8,5 +11,6 @@ uniform mat4 projection;
 
 void main()
 {
+  Color = color;
    gl_Position = projection * view * model * vec4(position, 1.0);
 }

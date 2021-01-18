4fab98601c3fcb86de28373adbbce696d919f6d4
Gwen Boatrite <gwen.boatrite@gmail.com>
2021-01-18 04:21:58 -0600

    Add new world page stub

--- src/renderers/new_world_page_renderer.cpp
+++ src/renderers/new_world_page_renderer.cpp
@@ -1,11 +1,9 @@
 #include "../font_book.h"
 #include "../util/imgui_util.h"
-#include "main_menu_renderer.h"
 #include "new_world_page_renderer.h"
-#include "root_renderer.h"
 
-MainMenuRenderer::MainMenuRenderer(std::shared_ptr<UIContext> ui_context_ptr) : m_ui_context_ptr{ui_context_ptr} {
-  std::cout << "MainMenuRenderer (" << this << ") created" << std::endl;
+NewWorldPageRenderer::NewWorldPageRenderer(std::shared_ptr<UIContext> ui_context_ptr) : m_ui_context_ptr{ui_context_ptr} {
+  std::cout << "NewWorldPageRenderer (" << this << ") created" << std::endl;
 
   // Create custom style for rendering window.
   m_style = ImGuiUtil::CreateStyle(
@@ -14,15 +12,13 @@ MainMenuRenderer::MainMenuRenderer(std::shared_ptr<UIContext> ui_context_ptr) : 
       style.WindowBorderSize = 0.0f;
     }
   );
-
-  m_ui_context_ptr->focusInGUI();
 }
 
-MainMenuRenderer::~MainMenuRenderer() {
-  std::cout << "MainMenuRenderer (" << this << ") destroyed" << std::endl;
+NewWorldPageRenderer::~NewWorldPageRenderer() {
+  std::cout << "NewWorldPageRenderer (" << this << ") destroyed" << std::endl;
 }
 
-void MainMenuRenderer::render(double dt) {
+void NewWorldPageRenderer::render(double dt) {
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
@@ -39,17 +35,7 @@ void MainMenuRenderer::render(double dt) {
   ImVec2 buttonSize(280, 40);
   ImGui::SetCursorPos(ImVec2(windowSize.x/2 - buttonSize.x/2, windowSize.y/2 - buttonSize.y/2)); // Move cursor on needed positions
   ImGui::PushFont(FontBook::NotoSans28Bold());
-  if (ImGui::Button("New World", buttonSize)) {
-    m_ui_context_ptr->changeCurrentPage(
-      std::make_shared<NewWorldPageRenderer>(m_ui_context_ptr)
-    );
-  }
-  ImGui::SetCursorPos(ImVec2(windowSize.x/2 - buttonSize.x/2, ImGui::GetCursorPos().y+10));
-  if (ImGui::Button("Test", buttonSize)) {
-    m_ui_context_ptr->changeCurrentPage(
-      std::make_shared<RootRenderer>(m_ui_context_ptr)
-    );
-  }
+  ImGui::Text("Placeholder");
   ImGui::PopFont();
   ImGui::End();
   ImGuiUtil::PopStyle();
---
+++ src/renderers/new_world_page_renderer.h
@@ -0,0 +1,19 @@
+# pragma once
+
+#include <memory>
+
+#include <imgui.h>
+
+#include "../ui_context.h"
+#include "renderer.h"
+
+class NewWorldPageRenderer : public Renderer {
+  private:
+    std::shared_ptr<UIContext> m_ui_context_ptr;
+    ImGuiStyle m_style;
+
+  public:
+    NewWorldPageRenderer(std::shared_ptr<UIContext> ui_context_ptr);
+    ~NewWorldPageRenderer();
+    void render(double dt) override;
+};

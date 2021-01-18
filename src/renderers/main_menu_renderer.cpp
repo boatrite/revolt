f323d7422cc0bb22e7e94aa2d58e26b6cfdbdf2e
Gwen Boatrite <gwen.boatrite@gmail.com>
2021-01-18 02:49:57 -0600

    Cleanup imgui main screen code
    Style on the outside, since that should affect everything. Then window
    code, then button.

--- src/renderers/main_menu_renderer.cpp
+++ src/renderers/main_menu_renderer.cpp
@@ -25,22 +25,16 @@ void MainMenuRenderer::render(double dt) {
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  ImGui::SetNextWindowPos(ImVec2(0, 0));
-  ImGui::SetNextWindowSize(ImVec2(m_ui_context_ptr->getWidth(), m_ui_context_ptr->getHeight()));
-  // ImGui::SetNextWindowSizeConstraints();
-  ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
-
-  const auto flags =
-    ImGuiWindowFlags_NoMove |
-    ImGuiWindowFlags_NoDecoration |
-    ImGuiWindowFlags_AlwaysAutoResize |
-    ImGuiWindowFlags_NoSavedSettings;
-
   ImGuiUtil::PushStyle(m_style);
-
-  // Render
-  ImGui::Begin("Main Menu", NULL, flags);
+  ImGui::SetNextWindowPos(ImVec2(0, 0));
   ImVec2 windowSize = ImGui::GetIO().DisplaySize;
+  ImGui::SetNextWindowSize(windowSize);
+  // ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+  ImGui::Begin("Main Menu", NULL,
+      ImGuiWindowFlags_NoMove |
+      ImGuiWindowFlags_NoDecoration |
+      ImGuiWindowFlags_AlwaysAutoResize |
+      ImGuiWindowFlags_NoSavedSettings);
   ImVec2 buttonSize(280, 40);
   ImGui::SetCursorPos(ImVec2(windowSize.x/2 - buttonSize.x/2, windowSize.y/2 - buttonSize.y/2)); // Move cursor on needed positions
   ImGui::PushFont(FontBook::NotoSans28Bold());
@@ -49,13 +43,11 @@ void MainMenuRenderer::render(double dt) {
   }
   ImGui::SetCursorPos(ImVec2(windowSize.x/2 - buttonSize.x/2, ImGui::GetCursorPos().y+10));
   if (ImGui::Button("Test", buttonSize)) {
-    std::cout << "Test button clicked." << std::endl;
     m_ui_context_ptr->changeCurrentPage(
       std::make_shared<RootRenderer>(m_ui_context_ptr)
     );
   }
   ImGui::PopFont();
   ImGui::End();
-
   ImGuiUtil::PopStyle();
 }
